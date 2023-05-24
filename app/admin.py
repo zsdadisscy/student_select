defae9cd2a29bd4c10b8f3d8d76ac94134c8075d
scy <sczruy@qq.com>
2023-05-24 23:26:35 +0800

    数据库结构优化，汉化

--- app/admin.py
+++ app/admin.py
@@ -5,14 +5,19 @@ from app.models import *
 
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'gender', 'email', 'phone', 'date_of_birth', 'student_type')
 
+
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'gender', 'email', 'phone', 'date_of_birth')
 
 
 admin.site.site_header = '硕士导师双选系统后台管理'
 admin.site.index_title = '后台管理'
 admin.site.site_title = '后台管理'
 
-admin.site.register(Student)
-admin.site.register(Tutor)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Tutor, TutorAdmin)
 admin.site.register(StudentTeacher)
 admin.site.register(UserLog)
--- app/models.py
+++ app/models.py
@@ -2,51 +2,27 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.contrib.auth.models import Group, Permission
 
-# 删除无用键，删除了权限，删除了无用键
-class Student(AbstractBaseUser):
-    username = models.CharField(max_length=10, blank=True)
-    id = models.CharField(max_length=10, primary_key=True)
-    date_of_birth = models.DateField()
-    gender = models.CharField(max_length=10, choices=[('man', '男'), ('women', '女')], default='man')
-    student_type = models.CharField(max_length=50, choices=[('special_master', '专硕'), ('learning_master', '学硕')], default='special_master')
-    bio = models.TextField()
-    email = models.EmailField(blank=False)
-    phone = models.CharField(max_length=20, blank=False)
-    is_selected = models.BooleanField(default=False)
-    select_limit = models.PositiveIntegerField(default=0)
-    select_count = models.PositiveIntegerField(default=0)
-    # groups = models.ManyToManyField(Group, related_name='%(class)s_groups')
-    # user_permissions = models.ManyToManyField(Permission, related_name='%(class)s_user_permissions')
-    photo = models.ImageField(upload_to='student_photo/', default='student_photo/student_photo.png')
-    USERNAME_FIELD = 'id'
 
-    def __str__(self):
-        return self.username
-
-    class Meta:
-        verbose_name = '学生'
-        verbose_name_plural = '学生'
-
-
+# 将权限删除，后期可拓展
 class Tutor(AbstractBaseUser):
-    username = models.CharField(max_length=10, blank=True)
-    id = models.CharField(max_length=10, primary_key=True)
+    username = models.CharField('姓名', max_length=10, blank=True)
+    id = models.CharField('工号', max_length=10, primary_key=True)
     USERNAME_FIELD = 'id'
-    date_of_birth = models.DateField()
-    gender = models.CharField(max_length=10, choices=[('man', '男'), ('women', '女')], default='man')
-    email = models.EmailField(blank=False)
-    phone = models.CharField(max_length=20, blank=False)
-    research_area = models.CharField(max_length=100)
-    bio = models.TextField()
-    is_recruiting = models.BooleanField(default=False, blank=False)
-    enrollment_type = models.CharField(max_length=50)
-    enrollment_limit = models.PositiveIntegerField(default=0)
-    enrollment_count = models.PositiveIntegerField(default=0)
-    selected_students = models.ForeignKey('Student', related_name='tutor_student', on_delete=models.CASCADE, null=True,
-                                          blank=True)
+    date_of_birth = models.DateField('出生日期')
+    gender = models.CharField('性别', max_length=10, choices=[('man', '男'), ('women', '女')], default='man')
+    email = models.EmailField('邮箱', blank=False)
+    phone = models.CharField('手机号', max_length=20, blank=False)
+    research_area = models.CharField('研究方向', max_length=100)
+    bio = models.TextField('简介')
+    is_recruiting = models.BooleanField('是否招收学生', default=False, blank=False)
+    enrollment_type = models.CharField('招生类型', max_length=50,
+                                       choices=[('special_master', '专硕'), ('learning_master', '学硕')])
+    enrollment_limit = models.PositiveIntegerField('招生上限', default=0)
+    enrollment_count = models.PositiveIntegerField('目前招生', default=0)
+
     # groups = models.ManyToManyField(Group, related_name='%(class)s_groups')
     # user_permissions = models.ManyToManyField(Permission, related_name='%(class)s_user_permissions')
-    photo = models.ImageField(upload_to='tutor_photo/', default='tutor_photo/tutor_photo.png')
+    photo = models.ImageField('头像', upload_to='tutor_photo/', default='tutor_photo/tutor_photo.png')
 
     def __str__(self):
         return self.username
@@ -56,11 +32,42 @@ class Tutor(AbstractBaseUser):
         verbose_name_plural = '导师'
 
 
+class Student(AbstractBaseUser):
+    username = models.CharField('姓名', max_length=10, blank=True)
+    id = models.CharField('学号', max_length=10, primary_key=True)
+    date_of_birth = models.DateField('出生日期')
+    gender = models.CharField('性别', max_length=10, choices=[('man', '男'), ('women', '女')], default='man')
+    student_type = models.CharField('学生类型', max_length=50,
+                                    choices=[('special_master', '专硕'), ('learning_master', '学硕')],
+                                    default='special_master')
+    bio = models.TextField('简介')
+    email = models.EmailField('邮箱', blank=False)
+    phone = models.CharField('手机号', max_length=20, blank=False)
+    is_selected = models.BooleanField('是否有老师选择', default=False)
+    select_limit = models.PositiveIntegerField('选择老师上线', default=0)
+    select_count = models.PositiveIntegerField('目前已选择的老师数目', default=0)
+    # groups = models.ManyToManyField(Group, related_name='%(class)s_groups')
+    # user_permissions = models.ManyToManyField(Permission, related_name='%(class)s_user_permissions')
+    photo = models.ImageField('头像', upload_to='student_photo/', default='student_photo/student_photo.png')
+    USERNAME_FIELD = 'id'
+    selected_tutor = models.ForeignKey('Tutor', verbose_name='导师', related_name='tutor_student',
+                                       on_delete=models.CASCADE, null=True,
+                                       blank=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = '学生'
+        verbose_name_plural = '学生'
+
+
 class StudentTeacher(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE, related_name='tutor')
-    tutor = models.ForeignKey('Tutor', on_delete=models.CASCADE, related_name='student')
-    relationship_status = models.CharField(max_length=20, choices=[('accepted', '已接受'), ('rejected', '已拒绝'),
-                                                                   ('pending', '待定')], default='pending')
+    student = models.ForeignKey('Student', verbose_name='学生', on_delete=models.CASCADE, related_name='tutor')
+    tutor = models.ForeignKey('Tutor', on_delete=models.CASCADE, verbose_name='导师', related_name='student')
+    relationship_status = models.CharField('状态', max_length=20,
+                                           choices=[('accepted', '已接受'), ('rejected', '已拒绝'),
+                                                    ('pending', '待定')], default='pending')
 
     class Meta:
         index_together = (('student', 'tutor'),)  # 如果需要支持组合查询，可以添加多个外键到Meta类中
@@ -73,9 +80,10 @@ class StudentTeacher(models.Model):
 
 
 class UserLog(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE, related_name='student_logs')
-    tutor = models.ForeignKey('Tutor', on_delete=models.CASCADE, related_name='tutor_logs')
+    student = models.ForeignKey('Student', verbose_name='学生', on_delete=models.CASCADE, related_name='student_logs')
+    tutor = models.ForeignKey('Tutor', verbose_name='导师', on_delete=models.CASCADE, related_name='tutor_logs')
     operation_type = models.CharField(
+        '操作类型',
         max_length=20,
         choices=(
             ('choose', '选择导师'),
